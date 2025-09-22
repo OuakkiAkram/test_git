c31e402bcb592a21fbbd7a1b85a62391bdcac704
AKRAM DEV <akramouak6@gmail.com>
2025-09-22 15:12:24 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let x = 0;
+
+for (x; x<=10; x++){
+    console.log(x);
+}
+
+const car = {type: "Fiat", model: "500", color: "White"};
+console.log(car.type);
+
+function getInputValue() {
+    const inputElement = document.getElementById("myInput");
+    const inputValue = inputElement.value;
+    console.log("Input Value:", inputValue);
+    alert("The value is: " + inputValue);
+}  
+
+
+
+
+console.log(inputValiue);
